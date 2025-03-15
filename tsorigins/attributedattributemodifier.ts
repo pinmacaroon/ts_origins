b2e8f106e46dbe6fb5856a8a6c55e831fad4866b
pinmacaroon <86179328+pinmacaroon@users.noreply.github.com>
2025-03-15 23:27:23 +0100

    Update attributedattributemodifier.ts
    writing ts on a phone is wild >v<

--- tsorigins/attributedattributemodifier.ts
+++ tsorigins/attributedattributemodifier.ts
@@ -9,4 +9,18 @@ export enum AttributedAttributeModifierOperation {
 export class AttributedAttributeModifier {
     public attribute: string;
     private operation: AttributedAttributeModifierOperation;
+    private value: number;
+    private name: string;
+
+    constructor(
+        attribute: string,
+        operation: AttributedAttributeModifierOperation,
+        value: number,
+        name?: string,
+    ){
+        this.attribute = attribute;
+        this.operation = operation;
+        this.value = value;
+        this.name = name ?? `${this.attribute} to ${this.value}`;
+    }
 }
